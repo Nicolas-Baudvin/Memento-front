a7f2da1b211e13b530eb0b2d553078127ed05434
Falorun <nicolas.baudvin@gmail.com>
2020-04-13 13:52:59 +0200

    Modification du style de l'ancre 'retour page d'accueil'

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,8 @@
 import React from "react";
-import "./style.scss";
 import { useHistory } from "react-router-dom";
+import { Icon } from 'semantic-ui-react';
+
+import "./style.scss";
 
 export default () => {
   const history = useHistory();
@@ -9,17 +11,18 @@ export default () => {
       <header className="about-header">
 
         <nav className="about-header-nav">
+          <Icon onClick={() => history.push("/")} name="arrow alternate circle left" size="big" color="blue" />
           <a onClick={() => history.push("/")} href="#" className="about-header-nav__item"> Retour Page d'Accueil </a>
         </nav>
 
-        <h2 className="about-header-simple">Simplicité</h2>
-        <h2 className="about-header-ergon">Ergonomie</h2>
-        <h2 className="about-header-realtime">Temps réel</h2>
+        <h2 className="about-header-simple"> <strong>Simplicité</strong></h2>
+        <h2 className="about-header-ergon"> <strong>Ergonomie</strong></h2>
+        <h2 className="about-header-realtime"> <strong>Temps réel</strong></h2>
 
       </header>
       <main className="about-body">
         <div className="rbn rbn-top rbn-right rbn-sticky rbn-green">à propos</div>
-        <h1 className="about-body-title">Mais qu'est donc Memento ?</h1>
+        <h1 className="about-body-title">Memento, qu'est ce que c'est ?</h1>
 
         <p className="about-body-text">
           <strong>Memento</strong> est un site, ou plutôt une <strong>application web</strong>  qui vous permet de créer des penses bêtes complexes et de les partager en temps réel avec votre famille, vos amis ou collègues.
