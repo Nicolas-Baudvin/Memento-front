8aa37be26ffbda868a8753050eb806a6f2f9a3b7
Falorun <nicolas.baudvin@gmail.com>
2020-04-14 15:48:24 +0200

    Authentification & Inscription Fonctionnelles

--- src/store/Registration/middleware.js
+++ src/store/Registration/middleware.js
@@ -1,40 +1,66 @@
 import axios from "axios";
+
+// utils
+import { decryptUserData, cryptUserData } from 'src/Utils/crypt';
+
+// actions
 import { SUBMIT_LOGIN_FORM, SUBMIT_SIGNUP_FORM } from "./actions";
+import { failMessage, successMessage } from "../Popup/actions";
 
 export default (store) => (next) => (action) => {
   const state = store.getState();
 
   switch (action.type) {
-
     case SUBMIT_LOGIN_FORM: {
-
       axios({
         method: "POST",
-        url: `${process.env.API_URL}auth/login/`,
+        url: `${process.env.LOGIN_API_URL}`,
         data: action.userData
       })
         .then((res) => {
           console.log(res);
+          store.dispatch(successMessage(res.data.message));
+
+          res.data.message = undefined;
+          action.userData = res.data;
+
+          const dataCrypted = cryptUserData(res.data);
+          localStorage.setItem("udta", dataCrypted);
         })
         .catch((err) => {
-          console.log(err);
+          console.log(err.response.data);
+
+          if (Array.isArray(err.response.data.errors)) {
+            const { msg } = err.response.data.errors[0];
+            store.dispatch(failMessage(msg));
+          }
+          else {
+            store.dispatch(failMessage(err.response.data.errors));
+          }
         });
 
       next(action);
       break;
     }
     case SUBMIT_SIGNUP_FORM: {
-
       axios({
         method: "POST",
-        url: `${process.env.API_URL}auth/signup/`,
+        url: `${process.env.SIGNUP_API_URL}`,
         data: action.userData
       })
         .then((res) => {
           console.log(res);
+          store.dispatch(successMessage(res.data.message));
         })
         .catch((err) => {
-          console.log(err);
+          console.log(err.response.data);
+          if (Array.isArray(err.response.data.errors)) {
+            const { msg } = err.response.data.errors[0];
+            store.dispatch(failMessage(msg));
+          }
+          else {
+            store.dispatch(failMessage(err.response.data.errors));
+          }
         });
 
       next(action);
--- src/store/Registration/reducer.js
+++ src/store/Registration/reducer.js
@@ -1,15 +1,19 @@
+import { decryptUserData } from 'src/Utils/crypt';
 import { SUBMIT_LOGIN_FORM, SUBMIT_SIGNUP_FORM } from "./actions";
 
 const initialState = {
-
+  datas: localStorage.getItem("udta") ? decryptUserData(localStorage.getItem("udta")) : false,
+  isConnected: localStorage.getItem("udta") ? !!decryptUserData(localStorage.getItem("udta")).token : false,
 };
 
 export default (state = initialState, action) => {
 
   switch (action.type) {
-
     case SUBMIT_LOGIN_FORM: {
-      return state;
+      return {
+        ...state,
+        datas: action.userData
+      };
     }
     case SUBMIT_SIGNUP_FORM: {
       return state;
