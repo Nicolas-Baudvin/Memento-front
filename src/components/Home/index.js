fbfcf807cfb1348909a25764c85c6932461f1790
Falorun <nicolas.baudvin@gmail.com>
2020-04-14 15:46:33 +0200

    retrait d'imports inutils

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.scss";
 
 // component
@@ -8,6 +8,7 @@ import { useHistory } from "react-router-dom";
 
 export default () => {
   const history = useHistory();
+
   return (
     <main className="home">
 
