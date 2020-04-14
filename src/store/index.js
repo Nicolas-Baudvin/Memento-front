19f008921260e1bccf11a6806633560e425ebcb1
Falorun <nicolas.baudvin@gmail.com>
2020-04-14 15:49:03 +0200

    Ajout des reducer au CombineReducer et middlewares à ApplyMiddlewares

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,14 @@ import { createStore, applyMiddleware, compose } from 'redux';
 // Logique de stockage et de manipulation des données de l'application.
 import reducer from './reducer';
 import mw from './middleware';
+import userMw from './Registration/middleware';
+import popupMw from './Popup/middleware';
 
 
 // applyMiddleware branche les middlewares de l'application au bon endroit
 // sur le trajet des actions qui sont dispatchées vers le store.
 // middlewares est par conséquent un "store enhancer".
-const middlewares = applyMiddleware(mw);
+const middlewares = applyMiddleware(mw, userMw, popupMw);
 
 const withReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,8 @@
 import { combineReducers } from 'redux';
+import userData from './Registration/reducer';
+import popup from './Popup/reducer';
 
-export default combineReducers({});
+export default combineReducers({
+  userData,
+  popup
+});
