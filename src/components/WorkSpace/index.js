b311b197ff34f55b7bc29eff34506ea06024ed9b
Falorun <nicolas.baudvin@gmail.com>
2020-04-15 18:32:10 +0200

    WIP: espace de travail

---
+++ src/components/WorkSpace/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.scss";
+
+// Components
+import Header from "src/components/Header";
+
+export default () => {
+  return (
+    <div className="workspace">
+      <Header />
+    </div>
+  );
+};
