1699e395c5e7f2ea7532ac2f0b5563109d36acee
Falorun <nicolas.baudvin@gmail.com>
2020-04-17 13:49:39 +0200

    Mise en place des actions redux pour les paramètres du compte utilisateur

--- src/store/Registration/actions.js
+++ src/store/Registration/actions.js
@@ -1,9 +1,37 @@
 export const SUBMIT_SIGNUP_FORM = "action/SUBMIT_SIGNUP_FORM";
 export const SUBMIT_LOGIN_FORM = "action/SUBMIT_LOGIN_FORM";
 export const LOGOUT = "action-LOGOUT";
+export const UPDATE_USERNAME = "action-UPDATE_USERNAME";
+export const UPDATE_EMAIL = "action-UPDATE_EMAIL";
+export const UPDATE_PASSWORD = "action-UPDATE_PASSWORD";
+export const FORGOT_PASSWORD = "action-FORGOT_PASSWORD";
+export const DELETE_ACCOUNT = "action-DELETE_ACCOUNT";
+
+export const updateUsername = (username) => ({
+  type: UPDATE_USERNAME,
+  username
+});
+
+export const updateEmail = (email) => ({
+  type: UPDATE_EMAIL,
+  email
+});
+
+export const updatePassword = (password) => ({
+  type: UPDATE_PASSWORD,
+  password
+});
+
+export const forgotPassword = () => ({
+  type: UPDATE_PASSWORD,
+});
+
+export const deleteAccount = () => ({
+  type: DELETE_ACCOUNT
+});
 
 export const logOut = () => ({
-  type: LOGOUT
+  type: LOGOUT,
 });
 
 export const submitSignupForm = (userData) => ({
