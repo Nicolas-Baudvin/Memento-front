a408853e3280045023242da025e479ef64eae67e
Falorun <nicolas.baudvin@gmail.com>
2020-04-17 13:43:18 +0200

    Array des items de la liste du menu paramètre

---
+++ src/Utils/navs.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+export const settingsNav = [
+  { title: "Mon Compte", icon: "user", key: 1 },
+  { title: "Changer de mot de passe", icon: "key", key: 2 },
+  { title: "Changer d'email", icon: "at", key: 3 },
+  { title: "Paramètres de compte", icon: "settings", key: 4 },
+  { title: "Supprimer mon compte", icon: "delete", key: 5 },
+];
