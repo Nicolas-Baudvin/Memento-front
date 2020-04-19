9ad28f8225dbfddfde58a91a997957d13d7a9e7d
Falorun <nicolas.baudvin@gmail.com>
2020-04-19 15:33:53 +0200

    suppression d'un middleware inutile

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 
 // Logique de stockage et de manipulation des données de l'application.
 import reducer from './reducer';
-import mw from './middleware';
 import userMw from './Registration/middleware';
 import popupMw from './Popup/middleware';
 import tabMw from './Tabs/middleware';
@@ -12,7 +11,7 @@ import tabMw from './Tabs/middleware';
 // applyMiddleware branche les middlewares de l'application au bon endroit
 // sur le trajet des actions qui sont dispatchées vers le store.
 // middlewares est par conséquent un "store enhancer".
-const middlewares = applyMiddleware(mw, userMw, popupMw, tabMw);
+const middlewares = applyMiddleware(userMw, popupMw, tabMw);
 
 const withReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
