596a456205fb0c01a767de149636557aeef409ab
Falorun <nicolas.baudvin@gmail.com>
2020-04-20 14:01:48 +0200

    Mise à jour de la config

--- src/setupTest.js
+++ src/setupTest.js
@@ -1,10 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
-// import Adapter from 'enzyme-adapter-react-16';
-// import chai from 'chai';
-// import { configure as configureEnzyme } from 'enzyme';
-// import createChaiEnzyme from 'chai-enzyme';
-// import createChaiJestDiff from 'chai-jest-diff';
-// import dirtyChai from 'dirty-chai';
 
 const Adapter = require("enzyme-adapter-react-16");
 const chai = require("chai");
@@ -13,11 +7,16 @@ const createChaiEnzyme = require("chai-enzyme");
 const chaiJestDiff = require("chai-jest-diff");
 const dirtyChai = require("dirty-chai");
 const sinonChai = require('sinon-chai');
+const chaiJestSnapshot = require('chai-jest-snapshot');
+const enzymeToJSON = require('enzyme-to-json/serializer');
 
 chai
   .use(chaiJestDiff.default())
   .use(dirtyChai)
   .use(createChaiEnzyme())
+  .use(chaiJestSnapshot)
   .use(sinonChai);
+
+expect.addSnapshotSerializer(enzymeToJSON);
 
 configureEnzyme({ adapter: new Adapter() });
