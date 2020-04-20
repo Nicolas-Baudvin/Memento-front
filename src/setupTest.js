18047abcceb2a78ae711f888af891e2305a3aa86
Falorun <nicolas.baudvin@gmail.com>
2020-04-20 12:50:52 +0200

    configuration des tests mis à jour

--- src/setupTest.js
+++ src/setupTest.js
@@ -12,10 +12,12 @@ const configureEnzyme = require("enzyme").configure;
 const createChaiEnzyme = require("chai-enzyme");
 const chaiJestDiff = require("chai-jest-diff");
 const dirtyChai = require("dirty-chai");
+const sinonChai = require('sinon-chai');
 
 chai
   .use(chaiJestDiff.default())
   .use(dirtyChai)
-  .use(createChaiEnzyme());
+  .use(createChaiEnzyme())
+  .use(sinonChai);
 
 configureEnzyme({ adapter: new Adapter() });
