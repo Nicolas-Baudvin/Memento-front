c5c657876cd53ec998204f8ed7a3bd61db553d0a
Falorun <nicolas.baudvin@gmail.com>
2020-04-21 14:58:22 +0200

    WIP: Mise en place du socket, et des limitations pour les Guests

--- src/components/WorkSpace/index.js
+++ src/components/WorkSpace/index.js
@@ -1,29 +1,82 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from 'react-router-dom';
+import { Input } from 'semantic-ui-react';
 import "./style.scss";
 
 // Components
 import Header from "../Header";
 
 // Actions
-import { newSocketTab } from "../../store/Socket/actions";
+import { newSocketTab, connectToTab } from "../../store/Socket/actions";
+import { newCurrentTab } from "../../store/Tabs/actions";
 
-export default () => {
+export default ({ isInvited }) => {
   const dispatch = useDispatch();
-  const { socketsList } = useSelector((globalState) => globalState.sockets);
-  const { id, name } = useParams();
+  const { currentSocket } = useSelector((globalState) => globalState.sockets);
+  const { currentTab } = useSelector((globalState) => globalState.mytabs);
+  const { userID } = useSelector((globalState) => globalState.userData.datas);
 
-  console.log(id);
+  /**
+   * @param link - pour invités seulement
+   * @param friendTabId - pour invités seulement
+   * */
+  const { id, name, link, friendTabId } = useParams();
+
+  const CopyToClipBoard = () => {
+    const copy = document.querySelector('.workspace-body-invitation').firstChild;
+    navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+      if (result.state === "granted" || result.state === "prompt") {
+        copy.select();
+        copy.setSelectionRange(0, 99999);
+
+        document.execCommand('Copy');
+      }
+    });
+  };
+
   useEffect(() => {
-    if (socketsList) {
+    if (!isInvited) {
+      console.log("Création d'une nouvelle instance...");
       dispatch(newSocketTab({ id, name }));
+    }
+    if (!isInvited) {
+      console.log("Recherche de la table en cours...");
+      dispatch(newCurrentTab(id));
+    }
+    if (isInvited) {
+      dispatch(connectToTab({ link, friendTabId }));
     }
   }, []);
 
   return (
-    <div data-tabid={id} className="workspace">
+    <div data-tabid={id} className="workspace" style={{ backgroundImage: `url(../../../${currentTab.imgPath})` }}>
       <Header />
+      <div className="workspace-body">
+        {
+          userID === currentTab.userID && <Input
+            className="workspace-body-invitation"
+            action={{
+              color: 'blue',
+              labelPosition: 'right',
+              icon: 'copy',
+              content: 'Copier',
+              onClick: CopyToClipBoard
+            }}
+            defaultValue={`http://localhost:3000/join/${currentTab._id}/${currentSocket.invitationLink}/`}
+          />
+        }
+        {
+          isInvited && <div className="workspace-body-invited">
+
+          </div>
+        }
+        {
+          !isInvited && <div className="workspace-body-">
+
+          </div>
+        }
+      </div>
     </div>
   );
 };
