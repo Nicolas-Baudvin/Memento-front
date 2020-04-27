3cdb4e1234cdb67b1e67db3f909f3d3ec0dba1f3
Falorun <nicolas.baudvin@gmail.com>
2020-04-27 15:27:51 +0200

    Création de listes désormais possible.

---
+++ src/components/WorkSpace/BodyHeader/index.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { Popup, Input } from 'semantic-ui-react';
+import { useSelector, useDispatch } from "react-redux";
+
+// Components
+import Guests from "../Guest";
+import Owner from '../Owner';
+import Menu from '../Menu';
+import { newList } from "../../../store/Lists/actions";
+
+export default ({ isInvited }) => {
+  const dispatch = useDispatch();
+  const { currentSocket } = useSelector((globalState) => globalState.sockets);
+  const { currentTab } = useSelector((globalState) => globalState.mytabs);
+  const { userID } = useSelector((globalState) => globalState.userData.datas);
+
+  const initialState = {
+    addlist: ''
+  };
+  const [state, setstate] = useState(initialState);
+
+  const copyToClipBoard = () => {
+    const copy = document.querySelector('.workspace-body-invitation').firstChild;
+    navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+      if (result.state === "granted" || result.state === "prompt") {
+        copy.select();
+        copy.setSelectionRange(0, 99999);
+
+        document.execCommand('Copy');
+      }
+    });
+  };
+
+  const handleAddListbtn = () => {
+    if (state.addlist && state.addlist.length < 30) dispatch(newList({ name: state.addlist, tabId: currentTab._id }));
+  };
+
+  return (
+    <div className="workspace-body-header">
+      <Menu className="workspace-body-header-menuBtn" isInvited={isInvited} />
+      {
+        userID === currentTab.userID && currentSocket && <Popup
+          content="C'est le liens qui te permettra d'inviter tes amis !"
+          trigger={<Input
+            className="workspace-body-invitation"
+            action={{
+              color: 'blue',
+              labelPosition: 'right',
+              icon: 'copy',
+              content: 'Copier',
+              onClick: copyToClipBoard
+            }}
+            defaultValue={`http://localhost:3000/join/${currentTab._id}/${currentSocket.invitationLink}/`}
+          />}
+        />
+      }
+      <Popup
+        content="C'est ici que tu vas créer ta liste. Appuie sur le bouton de gauche pour valider ton choix !"
+        trigger={
+          <Input
+            value={state.addlist}
+            onChange={(e) => setstate({ ...state, addlist: e.target.value })}
+            action={{
+              color: 'blue', icon: 'add', content: 'Ajouter une liste', onClick: handleAddListbtn
+            }}
+            actionPosition="left"
+            className="workspace-body-header-input"
+            placeholder="Nom de votre liste"
+          />
+        }
+      />
+      <div className="workspace-body-header-members">
+        {
+          currentSocket && <Owner currentSocket={currentSocket} isInvited={isInvited} />
+        }
+        <Guests guests={currentSocket.guests} isInvited={isInvited} />
+      </div>
+
+    </div>
+  );
+};
--- src/components/WorkSpace/index.js
+++ src/components/WorkSpace/index.js
@@ -1,33 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, useHistory } from 'react-router-dom';
-import { Input, Button, Popup } from 'semantic-ui-react';
-import cx from 'classnames';
 import "./style.scss";
 
 // Components
 import Header from "../Header";
 import List from "./List";
-import Guests from "./Guest";
-import Menu from './Menu';
-import Owner from './Owner';
+import BodyHeader from './BodyHeader';
 
 // Actions
 import { newSocketTab, connectToTab } from "../../store/Socket/actions";
 import { newCurrentTab } from "../../store/Tabs/actions";
 import { failMessage } from "../../store/Popup/actions";
+import { list } from "postcss";
+import { myLists } from "../../store/Lists/actions";
 
 export default ({ isInvited }) => {
-  const initialState = {
-    openAddList: false,
-    addlist: ''
-  };
+
   const dispatch = useDispatch();
   const history = useHistory();
-  const [state, setstate] = useState(initialState);
-  const { currentSocket, guests } = useSelector((globalState) => globalState.sockets);
   const { currentTab } = useSelector((globalState) => globalState.mytabs);
   const { userID } = useSelector((globalState) => globalState.userData.datas);
+  const { lists } = useSelector((GlobalState) => GlobalState.mylists);
   /**
    * @param link - pour invités seulement
    * @param friendTabId - pour invités seulement
@@ -36,98 +30,35 @@ export default ({ isInvited }) => {
     id, name, link, friendTabId
   } = useParams();
 
-  const CopyToClipBoard = () => {
-    const copy = document.querySelector('.workspace-body-invitation').firstChild;
-    navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
-      if (result.state === "granted" || result.state === "prompt") {
-        copy.select();
-        copy.setSelectionRange(0, 99999);
-
-        document.execCommand('Copy');
-      }
-    });
-  };
-
-  const handleAddListbtn = () => {
-    console.log(!state.addlist);
-    if (!state.addlist) {
-      setstate({ ...state, openAddList: !state.openAddList });
-    }
-    else {
-      console.log("nouvelle liste ...");
-    }
-  };
-
   useEffect(() => {
-    if (currentTab.userID === userID && !isInvited) {
-      if (!isInvited) {
+    dispatch(newCurrentTab(id));
+    if (Object.keys(currentTab).length) {
+      if (currentTab.userID === userID && !isInvited) {
         dispatch(newSocketTab({ id, name }));
       }
-      if (!isInvited) {
-        dispatch(newCurrentTab(id));
+      else {
+        history.push("/");
+      }
+      if (isInvited) {
+        dispatch(connectToTab({ link, friendTabId }));
       }
     }
-    else {
-      history.push("/");
-      return dispatch(failMessage("Vous ne pouvez pas accéder à un tableau qui ne vous appartient pas"));
-    }
-    if (isInvited) {
-      dispatch(connectToTab({ link, friendTabId }));
-    }
   }, []);
+
+  useEffect(() => {
+    console.log("rechercher des listes en cours...")
+    if (currentTab && !lists.length) {
+      console.log(currentTab._id);
+      dispatch(myLists(currentTab._id));
+    }
+  }, [currentTab]);
 
   return (
     <div data-tabid={id} className="workspace" style={{ backgroundImage: `url(../../../${currentTab && currentTab.imgPath})` }}>
       <Header />
       <div className="workspace-body">
-        <div className="workspace-body-header">
-          <Menu className="workspace-body-header-menuBtn" isInvited={isInvited} />
-          {
-            userID === currentTab.userID && currentSocket && <Popup
-              content="C'est le liens qui te permettra d'inviter tes amis !"
-              trigger={<Input
-                className="workspace-body-invitation"
-                action={{
-                  color: 'blue',
-                  labelPosition: 'right',
-                  icon: 'copy',
-                  content: 'Copier',
-                  onClick: CopyToClipBoard
-                }}
-                defaultValue={`http://localhost:3000/join/${currentTab._id}/${currentSocket.invitationLink}/`}
-              />}
-            />
-          }
-          <Popup
-            content="C'est ici que tu vas créer ta liste. Appuie sur le bouton de gauche pour valider ton choix !"
-            trigger={
-              <Input
-                value={state.addlist}
-                onChange={(e) => setstate({ ...state, addlist: e.target.value })}
-                action={{
-                  color: 'blue', icon: 'add', content: 'Ajouter une liste', onClick: handleAddListbtn
-                }}
-                actionPosition="left"
-                className={cx("workspace-body-header-input", { "addlist-active": state.openAddList })}
-                placeholder="Nom de votre liste"
-              />
-            }
-          />
-          <div className="workspace-body-header-members">
-            {
-              currentSocket && <Owner currentSocket={currentSocket} isInvited={isInvited} />
-            }
-            <Guests guests={currentSocket.guests} isInvited={isInvited} />
-          </div>
-
-        </div>
-        <List />
-        {
-          isInvited && <div className="workspace-body-invited" />
-        }
-        {
-          !isInvited && <div className="workspace-body-" />
-        }
+        <BodyHeader isInvited={isInvited} />
+        <List lists={lists} />
       </div>
     </div>
   );
