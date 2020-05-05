b93ea1028a4f2a5f9507a50f23f8e2e459ce3466
Falorun <nicolas.baudvin@gmail.com>
2020-05-05 14:50:32 +0200

    Création d'un composant Button perso

---
+++ src/Utils/Components/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+import { Icon } from 'semantic-ui-react';
+
+export const Button = (props) => {
+  const {
+    circular,
+    icon,
+    color,
+    iconSize,
+    border,
+  } = props;
+
+  const style = {
+    borderRadius: circular ? '50%' : '5px',
+    display: 'none',
+    alignItems: 'center',
+    padding: '.7em',
+    border: 'none',
+    backgroundColor: 'transparent',
+    color: color || "#fff",
+    border: border || '0px',
+  };
+
+  return (
+    <button {...props} style={style} type="button">
+      <Icon name={icon} size={iconSize || "normal"} />
+    </button>
+  );
+};
