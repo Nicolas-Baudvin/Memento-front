83af6a073d8ee9d0ea451b0503c666eaac00165d
unknown <nicolas.baudvin@gmail.com>
2020-05-18 21:20:47 +0200

    fix: cache les autres menu quand lorsqu'il est replié

--- src/components/WorkSpace/SideActionMenu/Actions/index.js
+++ src/components/WorkSpace/SideActionMenu/Actions/index.js
@@ -33,42 +33,46 @@ export default ({ state, setstate, isInvited }) => {
           <div />
         </div>
         {
-          state.view !== "last-actions" && <Popup
-            trigger={<Button onClick={handleClickChangeView('last-actions')} className="sideActionMenu-nav-btn" size="huge" icon="arrow alternate circle left" />}
-            content="Retour au menu principal"
-          />
+          state.menuIsOpen && <>
+            {
+              state.view !== "last-actions" && <Popup
+                trigger={<Button onClick={handleClickChangeView('last-actions')} className="sideActionMenu-nav-btn" size="huge" icon="arrow alternate circle left" />}
+                content="Retour au menu principal"
+              />
+            }
+            <Popup
+              trigger={<Button onClick={handleClickChangeView('chat')} size="huge" icon="wechat" color="vk" />}
+              content="Accéder au chat"
+            />
+            <Popup
+              trigger={<Button onClick={handleClickChangeView('tabInfo')} size="huge" icon="table" color="linkedin" />}
+              content="À propos de ce tableau"
+            />
+            {
+              !isInvited && <Popup
+                trigger={<Button icon="trash alternate" size="huge" color="red" />}
+                content="Supprimer le tableau"
+              />
+            }
+            <Popup
+              trigger={<Button onClick={() => history.push("/mentions-légales/")} icon="legal" size="huge" />}
+              content="Mentions légales"
+            />
+            <Popup
+              trigger={<Button onClick={() => history.push("/contact/")} icon="phone" size="huge" />}
+              content="Contact"
+            />
+            <Popup
+              trigger={<Button onClick={() => history.push("/a-propos/")} icon="question circle" size="huge" />}
+              content="À propos"
+            />
+            <Settings isOpen={state.isOpen} handleClose={handleClose} handleOpen={handleOpen} />
+            <Popup
+              trigger={<Button onClick={handleClickDisconnect} icon="power off" size="huge" />}
+              content="Déconnexion"
+            />
+          </>
         }
-        <Popup
-          trigger={<Button onClick={handleClickChangeView('chat')} size="huge" icon="wechat" color="vk" />}
-          content="Accéder au chat"
-        />
-        <Popup
-          trigger={<Button onClick={handleClickChangeView('tabInfo')} size="huge" icon="table" color="linkedin" />}
-          content="À propos de ce tableau"
-        />
-        {
-          !isInvited && <Popup
-            trigger={<Button icon="trash alternate" size="huge" color="red" />}
-            content="Supprimer le tableau"
-          />
-        }
-        <Popup
-          trigger={<Button onClick={() => history.push("/mentions-légales/")} icon="legal" size="huge" />}
-          content="Mentions légales"
-        />
-        <Popup
-          trigger={<Button onClick={() => history.push("/contact/")} icon="phone" size="huge" />}
-          content="Contact"
-        />
-        <Popup
-          trigger={<Button onClick={() => history.push("/a-propos/")} icon="question circle" size="huge" />}
-          content="À propos"
-        />
-        <Settings isOpen={state.isOpen} handleClose={handleClose} handleOpen={handleOpen} />
-        <Popup
-          trigger={<Button onClick={handleClickDisconnect} icon="power off" size="huge" />}
-          content="Déconnexion"
-        />
       </nav>
     </>
   );
