845493d45221efcc720f7bf90a1b32b5d8556e91
unknown <nicolas.baudvin@gmail.com>
2020-05-26 15:02:19 +0200

    Ajout d'un gestionnaire d'erreur

---
+++ src/Utils/Functions/AxiosErrorHandler.js
@@ -0,0 +1,18 @@
+import { failMessage } from "../../store/Popup/actions";
+import { logOut } from "../../store/Registration/actions";
+
+const errorHandler = (err, dispatch) => {
+  if (!err.response) {
+    return dispatch(failMessage("Une erreur est survenue sur le serveur. Réessayez ou contacter un administrateur"));
+  }
+  if (err.response.status === 401) {
+    dispatch(logOut());
+    return dispatch(failMessage("Votre session a expiré. Veuillez vous reconnecter."));
+  }
+  if (Array.isArray(err.response.data.errors)) {
+    return dispatch(failMessage(err.response.data.errors[0].msg));
+  }
+  return dispatch(failMessage(err.response.data.errors));
+};
+
+export default errorHandler;
