25e66d6fcd21d90daae07e3755c3f1ae58489703
unknown <nicolas.baudvin@gmail.com>
2020-06-07 21:34:25 +0200

    Liens oubli de mot de passe

--- src/components/Login/links.js
+++ src/components/Login/links.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./style.scss";
+import { useHistory } from "react-router-dom";
 
-export default ({ state, setstate }) => (
-  <>
-    <a className="form-link">Mot de passe oublié ?</a>
+export default ({ state, setstate }) => {
+  const history = useHistory();
+  return (<>
+    <a className="form-link" onClick={() => history.push("/oublie-mot-de-passe/")}>Mot de passe oublié ?</a>
     <a onClick={() => setstate({ ...state, currentView: "Signup" })} className="form-link">Tu n'as pas de compte ?</a>
   </>
-);
+  );
+};
