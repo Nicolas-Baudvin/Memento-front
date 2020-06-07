c1e95967ea087b98b2b369ef8056ec591889f54e
unknown <nicolas.baudvin@gmail.com>
2020-06-07 21:33:46 +0200

    Suppression de l'image && responsive

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,42 +2,13 @@ import React from "react";
 import "./style.scss";
 
 // component
-import { useHistory } from "react-router-dom";
 import Registration from "../Login";
 
-
-export default () => {
-  const history = useHistory();
-
-  return (
-    <main className="home">
-
-      <div className="home-left">
-
-        <div className="home-left-postit">
-          <h1 className="home-left-postit-title">Bienvenue sur Memento</h1>
-          <p>Votre pense-bête en ligne !</p>
-          <nav className="home-left-postit-nav">
-            <ul className="home-left-postit-nav-list">
-              <li onClick={() => history.push("/a-propos/")} className="home-left-postit-nav-list__item">
-                <a className="home-left-links"> à propos </a>
-              </li>
-              <li className="home-left-postit-nav-list__item">
-                <a className="home-left-links"> Mentions légales </a>
-              </li>
-              <li onClick={() => history.push("/contact/")} className="home-left-postit-nav-list__item">
-                <a className="home-left-links"> Contact </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-
-      </div>
-
-      <div className="home-right">
-        <Registration />
-      </div>
-
-    </main>
-  );
-};
+export default () => (
+  <main className="home">
+    <h1 className="home-title">Bienvenue sur Memento</h1>
+    <div className="home-registration">
+      <Registration />
+    </div>
+  </main>
+);
