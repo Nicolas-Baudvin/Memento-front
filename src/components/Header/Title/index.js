681a10465d9f8ddc3a6b9000659b442be6e718d6
unknown <nicolas.baudvin@gmail.com>
2020-06-09 21:25:48 +0200

    L'image est cliquable

--- src/components/Header/Title/index.js
+++ src/components/Header/Title/index.js
@@ -4,13 +4,13 @@ import { useHistory } from "react-router-dom";
 export default ({ state }) => {
   const history = useHistory();
   return (
-    <div className="workmenu-header-title">
+    <div onClick={() => history.push("/vos-tableaux/")} className="workmenu-header-title">
 
       {
         state.logo && <img className="workmenu-header-title-logo" src={state.logo} alt="logo" />
       }
 
-      <h1 onClick={() => history.push("/vos-tableaux/")}>Memento</h1>
+      <h1>Memento</h1>
     </div>
 );
 };
