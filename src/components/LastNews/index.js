59619e801b45c3707964613847a735a98593faa2
unknown <nicolas.baudvin@gmail.com>
2020-06-09 21:29:30 +0200

    Ajout des changelogs

---
+++ src/components/LastNews/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactMarkDown from 'react-markdown';
+import { Icon } from 'semantic-ui-react';
+import { useHistory } from 'react-router-dom';
+
+// Md file
+import ChangeLogs from '../../../changelog.md';
+// Styles
+import './style.scss';
+
+export default () => {
+  const history = useHistory();
+
+  const handleClickBackToHome = () => history.push("/");
+
+  return (
+    <div className="changelogs">
+      <a onClick={handleClickBackToHome} href=""> <Icon name="arrow left" /> Retour à l'accueil</a>
+      <ReactMarkDown source={ChangeLogs} escapeHtml={false} />
+    </div>
+  );
+};
