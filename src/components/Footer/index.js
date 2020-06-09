b631fd13451c351d68cd35e0bec7428d63cf40ae
unknown <nicolas.baudvin@gmail.com>
2020-06-09 21:29:18 +0200

    Ajout d'un footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import './style.scss';
+import { Popup } from 'semantic-ui-react';
+import { useHistory } from 'react-router-dom';
+import loadPic from '../../Utils/loadPic';
+
+const links = [
+  { name: "News", path: "/assets/news.webp" },
+  { name: "Contact", path: "/assets/mail.png" }
+];
+
+export default () => {
+  const [newsPic, setNewsPic] = useState();
+  const [mailPic, setMailPic] = useState();
+  const history = useHistory();
+
+  const getNewsPic = async (img) => {
+    const pic = await loadPic(img.path);
+    switch (img.name)
+    {
+      case "News": {
+        setNewsPic(pic);
+        break;
+      }
+      case "Contact": {
+        setMailPic(pic);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
+  const handleClickNewsPic = () => history.push("/nouveautes/");
+
+  useEffect(() => {
+    links.forEach((item) => {
+      getNewsPic(item);
+    });
+  }, []);
+
+  return (
+    <footer className="footer">
+      <div className="footer-block">
+        <Popup
+          trigger={<img onClick={handleClickNewsPic} src={newsPic} alt="changelogs" />}
+          content="Les dernières nouveautés"
+        />
+      </div>
+      <div className="footer-block">
+        <Popup
+          trigger={<a href="mailto:support@mymemento.fr"><img src={mailPic} alt="support@mymemento.fr" /></a>}
+          content="Un bug ? Contactez nous !"
+        />
+      </div>
+    </footer>
+  );
+};
