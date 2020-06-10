f2e7c1537ab8addbdd5b5f48f6ed840c4ec2fd31
unknown <nicolas.baudvin@gmail.com>
2020-06-10 21:23:40 +0200

    responsive

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -3,15 +3,13 @@ import { Icon } from 'semantic-ui-react';
 import cx from 'classnames';
 import "./style.scss";
 
-export default ({ message, isSuccess, isVisible }) => {
-  return (
-    <div className={cx("popupWindow", { "popupWindow-visible": isVisible })}>
-      <div className={cx("popupWindow-icon", { "icon-success": isSuccess })}>
-        <Icon name="info circle" size="huge" />
-      </div>
-      <div className={cx("popupWindow-text", { "text-success": isSuccess })}>
-        <p>{message}</p>
-      </div>
+export default ({ message, isSuccess, isVisible }) => (
+  <div className={cx("popupWindow", { "popupWindow-visible": isVisible })}>
+    <div className={cx("popupWindow-icon", { "icon-success": isSuccess })}>
+      <Icon name="info circle" size={window.screen.width <= 767 ? "big" : "huge"} />
     </div>
-  );
-};
+    <div className={cx("popupWindow-text", { "text-success": isSuccess })}>
+      <p>{message}</p>
+    </div>
+  </div>
+);
