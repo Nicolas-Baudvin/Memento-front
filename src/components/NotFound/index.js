a7ab4b57ec9df838d60ba0f18b0769e8ac89f8e9
unknown <nicolas.baudvin@gmail.com>
2020-06-10 19:09:09 +0200

    suppression de l'image, amélioration visuelle

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,62 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import "./style.scss";
-import loadPic from "../../Utils/loadPic";
 
 export default () => {
-  const initialState = {
-    img: ''
-  };
+
   const history = useHistory();
   const { isConnected } = useSelector((GlobalState) => GlobalState.userData);
-  const [state, setstate] = useState(initialState);
-
-  const pic = async (url) => {
-    try {
-      const img = await loadPic(url);
-      setstate({ ...state, img });
-    }
-    catch (e) {
-      console.log(e);
-    }
-  };
-
-  useEffect(() => {
-    pic("/assets/404-not-found.png");
-  }, []);
 
   return (
     <div className="notFound">
-      {
-        state.img && <img src={state.img} alt="404" />
-      }
+      <h1>404 Page Introuvable</h1>
       <h2>La page que vous avez demandé n'existe pas.</h2>
       Retourner à ...
       <nav>
         <ul>
           <li>
-            <a onClick={() => history.push("/a-propos/")}>à propos</a>
-          </li>
-
-          <li>
             <a onClick={() => history.push("/mentions-legales/")}>mentions légales</a>
-          </li>
-
-          <li>
-            <a onClick={() => history.push("/contact/")}>Contact</a>
           </li>
 
           <li>
             <a onClick={() => history.push("/")}>Page d'accueil</a>
           </li>
 
-          <li>
-            {
-              isConnected && <a onClick={() => history.push("/mon-espace-de-travail/")}>Mon espace de travail</a>
-            }
-          </li>
+          {
+            isConnected && <li>
+              <a onClick={() => history.push("/vos-tableaux/")}>Vos tableaux</a>
+            </li>
+          }
         </ul>
       </nav>
     </div>
