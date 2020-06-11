39b4bdb02a78f49614d40e8b4e71c2bf7a49bfc6
unknown <nicolas.baudvin@gmail.com>
2020-06-11 15:17:36 +0200

    seo: react helmet

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { Helmet } from 'react-helmet';
+
+// Styles
 import "./style.scss";
 
-// component
+// Components
 import Registration from "../Login";
 import Footer from '../Footer';
 
 
-export default ({ isConnected }) => (
+export default () => (
   <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Memento - Page de connexion</title>
+      <link rel="canonical" href="https://www.mymemento.fr/" />
+    </Helmet>
     <main className="home">
       <h1 className="home-title">Bienvenue sur <strong>My Memento</strong></h1>
       <h3 className="home-subtitle">Votre gestionnaire de listes de tâches !</h3>
