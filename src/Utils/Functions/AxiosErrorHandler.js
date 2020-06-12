4d4d945422ac49c30b93a8f3cd9a76346f99d5ae
unknown <nicolas.baudvin@gmail.com>
2020-06-12 15:31:38 +0200

    ajout de la redirection lors d'une erreur type 403

--- src/Utils/Functions/AxiosErrorHandler.js
+++ src/Utils/Functions/AxiosErrorHandler.js
@@ -1,13 +1,17 @@
 import { failMessage } from "../../store/Popup/actions";
 import { logOut } from "../../store/Registration/actions";
 
-const errorHandler = (err, dispatch) => {
+const errorHandler = (err, dispatch, history = []) => {
   if (!err.response) {
     return dispatch(failMessage("Erreur Interne. Réessayez ou contactez un administrateur."));
   }
   if (err.response.status === 401) {
     dispatch(logOut());
     return dispatch(failMessage(err.response.data.errors ? err.response.data.errors : "Votre session a expiré. Veuillez vous reconnecter."));
+  }
+  if (err.response.status === 403) {
+    history.push("/");
+    return dispatch(failMessage(err.response.data.errors));
   }
   if (Array.isArray(err.response.data.errors)) {
     return dispatch(failMessage(err.response.data.errors[0].msg));
