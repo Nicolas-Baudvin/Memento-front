21dc7ca085632c68df3edb7ae2498ca608cbfc97
unknown <nicolas.baudvin@gmail.com>
2020-06-20 20:07:33 +0200

    Ajout des titres + boutons

--- src/components/HomePage/Body/index.js
+++ src/components/HomePage/Body/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { Button, makeStyles } from '@material-ui/core';
+
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
+const useStyles = makeStyles(() => ({
+  button: {
+    margin: '1em 0',
+    backgroundColor: '#6E00C8',
+    color: '#fff',
+    fontSize: '2.5em',
+    fontWeight: '300',
+    '&:hover': {
+      backgroundColor: 'rgba(120, 0, 200, 1)'
+    }
+  },
+  icon: {
+    fontSize: '2em'
+  }
+}));
 
 export default () => {
+  const classes = useStyles();
   return (
     <div className="homePage-body">
       <div className="homePage-body-block">
         <div className="homePage-body-block-title">
           <h1><strong>Soyez Agile !</strong></h1>
           <h2><strong>My Memento</strong> vous aide dans votre organisation quotidienne ou professionnelle.</h2>
+          <Button endIcon={<ArrowForwardIcon className={classes.icon} />} className={classes.button} variant="contained">
+            C'est Parti !
+          </Button>
         </div>
       </div>
       <div className="homePage-body-block">
