ea4151a619347dbef470420281cf72b6f957d0f6
Falorun <nicolas.baudvin@gmail.com>
2020-06-23 13:27:15 +0200

    Responsive

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -2,15 +2,11 @@ import React from "react";
 import "./style.scss";
 
 import Header from './Header';
-import Footer from './Footer';
 import Body from './Body';
 
-export default () => {
-  return (
-    <div className="homePage">
-      <Header />
-      <Body />
-      <Footer />
-    </div>
-  );
-};
+export default () => (
+  <div className="homePage">
+    <Header />
+    <Body />
+  </div>
+);
