8baa12918b7cfed6e1d0cdf4070b45ff5abfb199
Falorun <nicolas.baudvin@gmail.com>
2020-06-26 14:14:33 +0200

    ajout d'un item 'theme'

--- src/Utils/navs.js
+++ src/Utils/navs.js
@@ -3,5 +3,6 @@ export const settingsNav = [
   { title: "Mon Compte", isSelected: true, key: 1 },
   { title: "Changer de mot de passe", isSelected: false, key: 2 },
   { title: "Changer d'email", isSelected: false, key: 3 },
-  { title: "Supprimer mon compte", isSelected: false, key: 5 },
+  { title: "Supprimer mon compte", isSelected: false, key: 4 },
+  { title: "Thèmes", isSelected: false, key: 5 },
 ];
